a44c4682c30234c1b9735c3002d0bc48f1eca21c
viniciuslemessouza <viniciuslemes314@gmail.com>
2026-04-16 18:31:46 -0300

    chore: create class Block

---
+++ game/block.py
@@ -0,0 +1,6 @@
+class Block:
+    def __init(self, x, y, color="green", outline_color="white"):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.outline_color = outline_color
