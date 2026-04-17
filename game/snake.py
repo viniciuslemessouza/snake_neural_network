1f8aa968abb3cf427e31c748c279af23a721e010
viniciuslemessouza <viniciuslemes314@gmail.com>
2026-04-17 10:25:40 -0300

    feat: add display instance

--- game/snake.py
+++ game/snake.py
@@ -1,10 +1,12 @@
 from player import Player
 from food import Food
+from display import Display
 
 class Game:
     def __init__(self):
         self.player = Player(0, 0)
         self.food = Food(0, 0)
+        self.display = Display(1200, 800, 200)
 
     def generate_player_position(self):
         pass
