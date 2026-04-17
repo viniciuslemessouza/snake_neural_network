013dbfa485a910cab2ed2b1bf04a8ee25f8f9afe
viniciuslemessouza <viniciuslemes314@gmail.com>
2026-04-17 10:46:55 -0300

    feat: add get_events function

--- game/display.py
+++ game/display.py
@@ -8,8 +8,16 @@ class Display:
         self.height = height
         self.hud_width = hud_width
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode(width+hud_width, height)
+        self.screen = pygame.display.set_mode([width+hud_width, height])
+        self.running = True
 
     def update(self):
+        self.get_events()
         self.screen.fill("black")
         pygame.display.flip()
+
+    def get_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                break
