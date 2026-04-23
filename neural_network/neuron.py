cd53614d67257637f17fa4f95f663aeaeea2adc8
viniciuslemessouza <viniciuslemes314@gmail.com>
2026-04-22 21:48:48 -0300

    chore: refactor weights range and activation function

--- neural_network/neuron.py
+++ neural_network/neuron.py
@@ -1,11 +1,11 @@
-import math
 import random
+import math
 
 class Neuron:
     def __init__(self, n_inputs):
-        self.weights = [random.gauss(0, math.sqrt(2 / n_inputs)) for _ in range(n_inputs)]
+        self.weights = [random.gauss(0, 1) for _ in range(n_inputs)]
         self.bias = random.gauss(0, 1)
-        self.activation = lambda x: max(0, x)
+        self.activation = lambda x: max(0, math.tanh(x))
 
     def forward(self, inputs):
         sign = sum(w * x  for w, x in zip(self.weights, inputs)) + self.bias
