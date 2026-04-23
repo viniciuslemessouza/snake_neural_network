f1f915c79adf0fa560682902cb8045d8111eed18
viniciuslemessouza <viniciuslemes314@gmail.com>
2026-04-23 14:55:46 -0300

    chore: move get events functions to snake module

--- game/snake.py
+++ game/snake.py
@@ -1,11 +1,13 @@
-from player import Player
-from food import Food
-from display import Display
+from game.player import Player
+from game.food import Food
+from game.display import Display, pygame
 
 FPS = 10
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 HUD_WIDTH = 200
+
+DISPLAY = Display(SCREEN_WIDTH, SCREEN_HEIGHT, HUD_WIDTH)
 
 class Game:
     def __init__(self):
@@ -41,12 +43,35 @@ class Game:
         DISPLAY.plot_text("Steps", self.player.steps, 3)
 
     def update(self):
-        DISPLAY.get_events(self.player)
+        self.get_events()
         self.player.update()
         DISPLAY.update()
 
+    def get_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if self.player.vx != 1:
+                        self.player.go_left()
+                        break
+                if event.key == pygame.K_RIGHT:
+                    if self.player.vx != -1:
+                        self.player.go_right()
+                        break
+                if event.key == pygame.K_UP:
+                    if self.player.vy != 1:
+                        self.player.go_up()
+                        break
+                if event.key == pygame.K_DOWN:
+                    if self.player.vy != -1:
+                        self.player.go_down()
+                        break
+            if event.type == pygame.QUIT:
+                DISPLAY.running = False
+                break
+
     def check_collisions(self):
-        if self.body_collision(self.player.rect) or self.wall_collision(self.player.x, self.player.y):
+        if self.body_collision() or self.wall_collision():
             self.over()
         if self.food_collision():
             self.got_food()
@@ -60,16 +85,14 @@ class Game:
     def food_collision(self):
         return self.player.rect == self.food.rect
 
-    def body_collision(self, rect):
-        return rect in self.player.body[:-1]
+    def body_collision(self):
+        return self.player.rect in self.player.body[:-1]
 
-    @staticmethod
-    def wall_collision(x, y):
-        return not 0 <= x < SCREEN_WIDTH or not 0 <= y < SCREEN_HEIGHT
+    def wall_collision(self):
+        return not 0 <= self.player.x < SCREEN_WIDTH or not 0 <= self.player.y < SCREEN_HEIGHT
 
     def over(self):
         self.__init__()
 
 if __name__ == "__main__":
-    DISPLAY = Display(SCREEN_WIDTH, SCREEN_HEIGHT, HUD_WIDTH)
     Game().play()
