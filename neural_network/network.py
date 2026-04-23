0d6b87dcd4da70cb186c642b3f4454728b299704
viniciuslemessouza <viniciuslemes314@gmail.com>
2026-04-22 21:50:44 -0300

    feat: create class Network

---
+++ neural_network/network.py
@@ -0,0 +1,18 @@
+from neuron import Neuron
+
+class Network:
+    def __init__(self, structure):
+        self.structure = structure
+        self.net = []
+        self.output = 0
+
+    def run(self, inputs):
+        for neurons_per_layer in self.structure:
+            layer = []
+            self.output = []
+            for _ in range(neurons_per_layer):
+                neuron = Neuron(len(inputs))
+                layer.append(neuron)
+                self.output.append(neuron.forward(inputs))
+            inputs = self.output
+            self.net.append(layer)
