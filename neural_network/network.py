17dd2c7e9a83615eb7ddcde0ccce59e17db52085
viniciuslemessouza <viniciuslemes314@gmail.com>
2026-04-24 08:29:57 -0300

    chore: separate build net function

--- neural_network/network.py
+++ neural_network/network.py
@@ -3,17 +3,24 @@ from neural_network.neuron import Neuron
 class Network:
     def __init__(self, structure):
         self.structure = structure
-        self.net = []
-        self.output = 0
+        self.net = self.build_net()
+        self.output = []
         self.fitness = 0
 
-    def run(self, inputs):
+    def build_net(self):
+        net = []
+        n_inputs = self.structure[0]
         for neurons_per_layer in self.structure:
             layer = []
-            self.output = []
             for _ in range(neurons_per_layer):
-                neuron = Neuron(len(inputs))
-                layer.append(neuron)
+                layer.append(Neuron(n_inputs))
+            net.append(layer)
+            n_inputs = neurons_per_layer
+        return net
+
+    def run(self, inputs):
+        for layer in self.net:
+            self.output = []
+            for neuron in layer:
                 self.output.append(neuron.forward(inputs))
             inputs = self.output
-            self.net.append(layer)
