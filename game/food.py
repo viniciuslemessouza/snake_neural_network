32cdf5844982d4b466642304f96dbef7f201af35
viniciuslemessouza <viniciuslemes314@gmail.com>
2026-04-26 20:27:51 -0300

    chore: change random module import method

--- game/food.py
+++ game/food.py
@@ -1,5 +1,4 @@
-import random
-
+from random import randint
 from game.block import Block
 
 class Food(Block):
@@ -8,8 +7,8 @@ class Food(Block):
 
     def set_position(self, screen_width, screen_height, exceptions):
         while True:
-            self.x = random.randint(0, screen_width // self.tile - 1) * self.tile
-            self.y = random.randint(0, screen_height // self.tile - 1) * self.tile
+            self.x = randint(0, screen_width // self.tile - 1) * self.tile
+            self.y = randint(0, screen_height // self.tile - 1) * self.tile
             self.get_rect()
             if not self.rect in exceptions:
                 break
